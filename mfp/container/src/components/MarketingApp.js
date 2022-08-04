2284af81dccf82c4d079bc0b0053af9d955c4621
MechanicalProject <abdbscd2@naver.com>
2022-08-04 13:30:59 +0900

    [MFE] Multi-tier navigation 적용

--- mfp/container/src/components/MarketingApp.js
+++ mfp/container/src/components/MarketingApp.js
@@ -1,12 +1,25 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+
+        // 아래 조건이 없으면, pathname === nextPathname 인 경우 무한 루프 발생!
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref}></div>;
 };
